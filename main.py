dd29d3e4c7c0cc2014a522ca6cabf68fcaa3ffc3
EliPadgett <eli.padgett115@gmail.com>
2026-04-02 16:31:10 -0500

    E, L, half of I

---
+++ main.py
@@ -0,0 +1,79 @@
+from turtle import *
+
+screen = Screen()
+
+screen.title("Write your name")
+screen.bgcolor("Black")
+screen.setup(width= 1000, height= 600)
+
+yertle = Turtle()
+yertle.color("yellow")
+yertle.shape("turtle")
+
+# for j in range(4):
+#     for i in range(5):
+#         yertle.forward(10)
+#         yertle.penup()
+#         yertle.forward(10)
+#         yertle.pendown()
+#     yertle.left(90)
+
+# while True:
+#     angle = int(input("What direction do you want Yertle to face?"))
+#     yertle.setheading(angle)
+
+# yertle.pu()
+# yertle.goto(-100, -100)
+# yertle.pd()
+# yertle.sety(100)
+# yertle.setx(100)
+# yertle.sety(-100)
+# yertle.setx(-100)
+# yertle.home()
+
+# for i in range(360):
+#     yertle.forward(1)
+#     yertle.right(1)
+
+# yertle.circle(-50, 180)
+
+yertle.pu()
+yertle.goto(-300, 0)
+yertle.pd()
+yertle.goto(-300,100)
+yertle.goto(-250,100)
+yertle.pu()
+yertle.goto(-300,50 )
+yertle.pd()
+yertle.goto(-250,50)
+yertle.pu()
+yertle.goto(-300, 0)
+yertle.pd()
+yertle.goto(-250,0)
+yertle.pu()
+yertle.goto(-200, 0)
+yertle.pd()
+yertle.goto(-200, 100)
+yertle.pu()
+yertle.goto(-150, 0)
+yertle.pd()
+yertle.goto(-150, 75)
+yertle.pu()
+yertle.goto(-150,100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
